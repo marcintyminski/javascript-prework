e389c45c05f86904b92c927a91fb991bb4a8b0e1
Marcin Tyminski <tymin12@o2.pl>
2021-05-07 18:33:29 +0200

    Add computer move

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// ruch komputera
+
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if (randomNumber == 1) {
+    computerMove = 'kamień';
+} else if (randomNumber == 2) {
+    computerMove = 'papier';
+} else {
+    computerMove = 'nożyce';
+}
+
+printMessage('Mój ruch to ' + computerMove);
